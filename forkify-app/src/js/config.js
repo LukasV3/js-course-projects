67f8ff448dd46215783d6f78c5b7c526f79d1288
LukasV3 <volk.lukas237@yahoo.co.uk>
2020-11-23 17:23:13 +0000

    Add existing file

---
+++ forkify-app/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "1995fde6-e82f-452d-b6d1-caca70970bab";
+export const MODAL_CLOSE_SECONDS = 2.5;
